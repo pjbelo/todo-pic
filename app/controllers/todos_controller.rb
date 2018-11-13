a2c0b4dfc198a551e9600fdb9dc39f2a3fdc7ee1
Paulo Belo <pjbelo@gmail.com>
2018-11-13 19:19:44 +0000

    setup one attachment

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -69,6 +69,6 @@ class TodosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def todo_params
-      params.require(:todo).permit(:task)
+      params.require(:todo).permit(:task, :picture)
     end
 end
